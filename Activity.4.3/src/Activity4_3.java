010953d9a0d6b4a779bd62462dfa518bf68714d0
jwinkler2005 <josh.s.winkler@gmail.com>
2013-11-07 19:45:05 -0500

    New Project: CSC130 Assignment 4.3
    Online Discussion portion
    
    Signed-off-by: Joshua Winkler <josh.s.winkler@gmail.com>

---
+++ Activity.4.3/src/Activity4_3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Activity4_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> userNums = new LinkedList<Integer>();
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Please enter 3 non negative integers.");
+		
+		for(int x = 1; x < 4; x++ ){
+			System.out.print("Enter Integer " + x + ": ");
+			userNums.add(Integer.parseInt(userInput.next()));
+		}
+		Collections.sort(userNums);
+		
+		System.out.print("The numbers in increasing order are: ");
+		for (int i : userNums){
+			System.out.print(i + " ");
+		}
+	}
+
+}
